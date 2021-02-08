341e96290ca88f2c4730c4e7429cafc67f4cb1a9
Austin Parker <austin@lightstep.com>
2021-02-08 19:55:28 +0000

    tweak

--- otel-collector.sh
+++ otel-collector.sh
@@ -4,10 +4,12 @@ if [ "${CODESPACES}" != "true" ]; then
     exit 0
 fi
 
-sed -i -e 's/TOKEN/$LS_ACCESS_TOKEN/g' .collector-config.yaml
+sed -i -e 's/TOKEN/$LS_ACCESS_TOKEN/g' collector-config.yaml
+ln -s collector-config.yaml $HOME/collector-config.yaml
 
 OTEL_COLLECTOR_VERSION=0.19.0
 OTEL_COLLECTOR_URL=https://github.com/open-telemetry/opentelemetry-collector-contrib/releases/download/v$OTEL_COLLECTOR_VERSION/otelcontribcol_linux_amd64
 OTEL_COLLECTOR_PATH=/var/tmp/otelcontribcol
 wget $OTEL_COLLECTOR_URL -O $OTEL_COLLECTOR_PATH
+chmod +x $OTEL_COLLECTOR_PATH
 /var/tmp/otelcontribcol --config=~/.collector-config.yaml & disown
