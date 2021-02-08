95c0d2ea8b001823050036b81d197a1957d3d3b8
Austin Parker <austin@lightstep.com>
2021-02-08 18:54:22 +0000

    tweak

--- otel-collector.sh
+++ otel-collector.sh
@@ -9,6 +9,6 @@ sed -i -e 's/TOKEN/$LS_ACCESS_TOKEN/g' .collector-config.yaml
 OTEL_COLLECTOR_VERSION="0.19.0"
 OTEL_COLLECTOR_URL="https://github.com/open-telemetry/opentelemetry-collector-contrib/releases/download/v$OTEL_COLLECTOR_VERSION/otel-contrib-collector_$OTEL_COLLECTOR_VERSION_amd64.deb"
 OTEL_COLLECTOR_PATH="/var/otelcontribcol/otel-contrib-collector.deb"
-wget OTEL_COLLECTOR_URL OTEL_COLLECTOR_PATH
-sudo apt install OTEL_COLLECTOR_PATH
+wget $OTEL_COLLECTOR_URL $OTEL_COLLECTOR_PATH
+sudo apt install $OTEL_COLLECTOR_PATH
 otelcontribcol --config=~/.collector-config.yaml & disown
